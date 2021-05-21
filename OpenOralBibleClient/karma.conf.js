7ebea154ad6083cef6ac046cf9d7c4e49d746433
Chris Meek <m33ksauce@gmail.com>
2021-05-20 22:36:12 -0500

    Trying slight changes

--- OpenOralBibleClient/karma.conf.js
+++ OpenOralBibleClient/karma.conf.js
@@ -26,6 +26,7 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
+    browserNoActivityTimeout: 30000,
     singleRun: false
   });
 };
