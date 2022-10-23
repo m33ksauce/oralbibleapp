7aec86b776a694f4371d03a953191a414d6809b9
Chris Meek <m33ksauce@gmail.com>
2022-10-23 00:05:54 -0500

    Update env and initial bundle

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   backend: {
-    releaseEndpoint: "https://us-central1-oralbibleapp.cloudfunctions.net/handler/api/v1/release",
-    audioEndpoint: "https://us-central1-oralbibleapp.cloudfunctions.net/handler/api/v1/audio",
+    releaseEndpoint: "https://content.oralbible.app/api/v1/release",
+    audioEndpoint: "https://content.oralbible.app/api/v1/audio",
   },
   features: {
     dynamicContent: true,
