600dff9780e8b26864098e967c8404101db6e3a8
Chris <m33ksauce@gmail.com>
2022-10-29 14:35:37 -0500

    Let menus close if media is playing (#33)

--- client/src/environments/environment.prod.ts
+++ client/src/environments/environment.prod.ts
@@ -7,5 +7,6 @@ export const environment = {
   features: {
     dynamicContent: true,
     bluetoothUpdate: false,
+    mediaCanCollapseWhenPlaying: true,
   }
 };
--- client/src/environments/environment.ts
+++ client/src/environments/environment.ts
@@ -11,6 +11,7 @@ export const environment = {
   features: {
     dynamicContent: true,
     bluetoothUpdate: false,
+    mediaCanCollapseWhenPlaying: true,
   }
 };
 
