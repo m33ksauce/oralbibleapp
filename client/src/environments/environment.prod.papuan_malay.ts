9e1e26ee6e6f12ae8efeb109766b67d7b107efbc
Chris Meek <m33ksauce@gmail.com>
2023-01-03 22:03:49 -0600

    Add papuan malay env

--- client/src/environments/environment.prod.papuan_malay.ts
+++ client/src/environments/environment.prod.papuan_malay.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   backend: {
-    releaseEndpoint: "https://content.oralbible.app/api/v1/yetfa/release",
-    audioEndpoint: "https://content.oralbible.app/api/v1/yetfa/audio",
+    releaseEndpoint: "https://content.oralbible.app/api/v1/papuan_malay/release",
+    audioEndpoint: "https://content.oralbible.app/api/v1/papuan_malay/audio",
   },
   features: {
     dynamicContent: true,
